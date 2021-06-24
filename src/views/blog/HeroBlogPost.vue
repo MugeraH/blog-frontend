e7cad0bfa36fb16a7454f4fe87a14ccbf7b10db6
MugeraH <bwosihughes24@gmail.com>
2021-06-24 05:42:23 +0300

    add function to formart date

--- src/views/blog/HeroBlogPost.vue
+++ src/views/blog/HeroBlogPost.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="wrapper">
-    <!-- <img :src="require(`{{post.image_url}}`)" alt="image" /> -->
+    <img :src="require(`${post.image_url}`)" alt="image" />
 
     <h2>{{ post.title }}</h2>
 
     <div class="centered">
       <span>
-        {{ post.created_at }}
+        {{ callDate(post.created_at, "fullDate") }}
       </span>
     </div>
   </div>
